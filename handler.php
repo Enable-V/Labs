50f3ffa4715a7a4ff928e81b4ce9529fd77faf2c
Alexey <ads>
2021-09-09 20:14:49 +0300

    d

---
+++ handler.php
@@ -0,0 +1,24 @@
+<?php
+    $stpHeading = "<h1>Привет, ".$_POST["username"]."</h1>";
+    switch ($_POST["favoritecolor"]) {
+        case "r":
+            $strBackgroundColor = "rgb(255,0,0)";
+            break;
+        case "g":
+            $strBackgroundColor = "rgb(0,255,0)";
+            break;
+        case "b":
+            $strBackgroundColor = "rgb(0,0,255)";
+        default:
+            $strBackgroundColor = "rgb(255,255,255)";
+            break;
+    }
+    ?>
+<html>
+<head>
+    <title>Форма</title>
+</head>
+<body style="background: <?php echo $strBackgroundColor;?>;">
+    <?php echo $stpHeading ?>
+</body>
+</html>
