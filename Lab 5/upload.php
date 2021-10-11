40cd9d1558b64b39cf9833410038412d8358bfaf
student <231@mail.ru>
2021-10-11 11:28:40 +0300

    Лаба 5 сделано до заданий

--- Lab 5/upload.php
+++ Lab 5/upload.php
@@ -8,6 +8,10 @@
 <h1>Загрузка изображения с измнением размеров</h1>
 <?php
 //Пути загрущки файлов
+//if(!is_dir("i/" )&!is_dir("tmp/" ) ) {
+// mkdir("i/");
+// mkdir("tmp/");
+//}
 $path = 'i/';
 $tmp_path = 'tmp/';
 //Массив допустимых значений
@@ -18,8 +22,8 @@ $size = 1024000;
 //Обработка запроса
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //Проверям тип файла
-    if (!in_array($_FILES['picture']['types'], $types)) ;
-    die('<p>Запрещенный тип файла. <a href="?">Попробовать другой файл?</a></p>');
+    if (!in_array($_FILES['picture']['type'], $types))
+        die('<p>Запрещенный тип файла. <a href="?">Попробовать другой файл?</a></p>');
 
     //Проверям размер файла
     if ($_FILES['picture']['size'] > $size)
@@ -33,78 +37,79 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     function resize($file, $type = 1, $rotate = null, $quality = null)
     {
         global $tmp_path;
+
+        //Ограничение по ширине в пикселях
+        $max_thumb_size = 200;
+        $max_size = 600;
+        //Качество изображения по умолчанию
+        if ($quality == null)
+            $quality = 75;
+
+        //Создаем исходное изображение на основе исходного файла
+        if ($file['type'] == 'image/jpeg')
+            $source = imagecreatefromjpeg($file['tmp_name']);
+        elseif ($file['type'] == 'image/png')
+            $source = imagecreatefrompng($file['tmp_name']);
+        elseif ($file['type'] == 'image/gif')
+            $source = imagecreatefromgif($file['tmp_name']);
+        else
+            return false;
+        //Поворачиваем изображение
+        if ($rotate != null)
+            $src = imagerotate($source, $rotate, 0);
+        else
+            $src = $source;
+
+        //Определяем ширину и высоту изображения
+        $w_src = imagesx($src);
+        $h_src = imagesy($src);
+
+        //В зависимости от типа (эскиз или большое изоборажение) устанавливает ограничение по ширине.
+        if ($type == 1)
+            $w = $max_thumb_size;
+        elseif ($type == 2)
+            $w = $max_size;
+
+        $dest = null;
+        //Если ширина больше заданной
+        if ($w_src > $w) {
+            //Вычисление пропорций
+            $ratio = $w_src / $w;
+            $w_dest = round($w_src / $ratio);
+            $h_dest = round($h_src / $ratio);
+
+            //Создаем пустую картинку
+            $dest = imagecreatetruecolor($w_dest, $h_dest);
+            //Копируем старое изображение в новое с изменением параметров
+            imagecopyresampled($dest, $src, 0, 0, 0, 0, $w_dest, $h_dest, $w_src, $h_src);
+
+            //Вывод картинки и очистка памяти
+            imagejpeg($dest, $tmp_path . $file['name'], $quality);
+            imagedestroy($dest);
+            imagedestroy($src);
+
+            return $file['name'];
+        } else {
+            //Вывод картинки и очистка памяти
+            imagejpeg($dest, $tmp_path . $file['name'], $quality);
+            imagedestroy($src);
+
+            return $file['name'];
+        }
     }
 
-    //Ограничение по ширине в пикселях
-    $max_thumb_size = 200;
-    $max_size = 600;
-    //Качество изображения по умолчанию
-    if ($quality == null)
-        $quality = 75;
+    //Загрузка файла и вывод сообщения
+    $_POST['file_rotate'] = isset($_POST['file_rotate']) ? $_POST['file_rotate'] : null;
+    $name = resize($_FILES['picture'], $_POST['file_type'], $_POST['file_rotate']);
 
-    //Создаем исходное изображение на основе исходного файла
-    if ($file['type'] == 'image/jpeg')
-        $source = imagecreatefromjpeg($file['tmp_name']);
-    elseif ($file['type'] == 'image/png')
-        $source = imagecreatefrompng($file['tmp_name']);
-    elseif ($file['type'] == 'image/gif')
-        $source = imagecreatefromgif($file['tmp_name']);
+    if (!@copy($tmp_path . $name, $path . $name))
+        echo '<p>Что-то пошло не так </p>';
     else
-        return false;
-    //Поворачиваем изображение
-    if ($rotate != null)
-        $src = imagerotate($source, $rotate, 0);
-    else
-        $src = $source;
+        echo '<p>Загрузка удачно прошла удачно <a href="' . $path . $_FILES['picture']['name'] . '">Посмотреть</a>.</p>';
 
-    //Определяем ширину и высоту изображения
-    $w_src = imagesx($src);
-    $h_src = imagesy($src);
-
-    //В зависимости от типа (эскиз или большое изоборажение) устанавливает ограничение по ширине.
-    if ($type == 1)
-        $w = $max_thumb_size;
-    elseif ($type == 2)
-        $w = $max_size;
-
-    //Если ширина больше заданной
-    if ($w_src > $w) {
-        //Вычисление пропорций
-        $ratio = $w_src / $w;
-        $w_dest = round($w_src / $ratio);
-        $h_dest = round($h_src / $ratio);
-
-        //Создаем пустую картинку
-        $dest = imagecreatetruecolor($w_dest, $h_dest);
-        //Копируем старое изображение в новое с изменением параметров
-        imagecopyresampled($dest, 0, 0, 0, 0, $w_dest, $h_dest, $w_src, $h_src);
-
-        //Вывод картинки и очистка памяти
-        imagejpeg($dest, $tmp_path . $file['name'], $quality);
-        imagedestroy($dest);
-        imagedestroy($src);
-
-        return $file['name'];
-    } else {
-        //Вывод картинки и очистка памяти
-        imagejpeg($dest, $tmp_path . $file['name'], $quality);
-        imagedestroy($src);
-
-        return $file['name'];
-    }
+    //Удаляем временный файл
+    unlink($tmp_path . $name);
 }
-
-$name = resize($_FILES['picture'], $_POST['file_type'], $_POST['file_rotate']);
-
-//Загрузка файла и вывод сообщения
-if (!@copy($tmp_path . $name, $path . $name))
-    echo '<p>Что-то пошло не так </p>';
-else
-    echo '<p>Загрузка удачно прошла удачно <a href="' . $path . $_FILES['picture']['name'] . '">Посмотреть</a>.</p>';
-
-//Удаляем временный файл
-
-unlink($tmp_path . $name)
 ?>
 
 <form method="post" enctype="multipart/form-data">
@@ -114,8 +119,9 @@ unlink($tmp_path . $name)
     <br>
     <select name="file_type">
         <option value="1">Эскиз</option>
-        <option value="2"
+        <option value="2">Большое изображение</option>
     </select>
+    <input type="submit" value="Загрузить">
 </form>
 </body>
 </html>
